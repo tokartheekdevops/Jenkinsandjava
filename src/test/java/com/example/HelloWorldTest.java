70d87e7ad0bc460e8d97695b71712a2299553ee2
tokartheekdevops <tokartheekdevops@gmail.com>
2026-08-17 12:50:03 +0530

    Add unit test for HelloWorld class

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld() {
+
+        String expected = "Hello, World!";
+
+        String actual = "Hello, World!";
+
+        assertEquals(expected, actual);
+    }
+}
